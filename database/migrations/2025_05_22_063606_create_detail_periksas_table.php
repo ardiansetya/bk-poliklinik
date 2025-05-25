69d0df85436572c1406b7361213d566649786ab7
ardiansetya <ardiansetya129@gmail.com>
2025-05-25 16:47:51 +0700

    fix foreign id detail periksas

--- database/migrations/2025_05_22_063606_create_detail_periksas_table.php
+++ database/migrations/2025_05_22_063606_create_detail_periksas_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('detail_periksas', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('id_periksa')->constrained('janji_periksas')->onDelete('cascade');
+            $table->foreignId('id_periksa')->constrained('periksas')->onDelete('cascade');
             $table->foreignId('id_obat')->constrained('obats');
             $table->timestamps();
         });
