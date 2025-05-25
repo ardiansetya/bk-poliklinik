77d1e70ea65d958cdf2d3775d7187850ffa57aad
ardiansetya <ardiansetya129@gmail.com>
2025-05-25 17:36:58 +0700

    fix role user migration

--- app/Models/User.php
+++ app/Models/User.php
@@ -18,9 +18,15 @@ class User extends Authenticatable
      * @var list<string>
      */
     protected $fillable = [
-        'name',
+        'nama',
+        'alamat',
+        'no_ktp',
+        'no_hp',
+        'no_rm',
+        'poli',
         'email',
         'password',
+        'role',
     ];
 
     public function jadwalPeriksas(){
--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
+            $table->enum('role', ['dokter', 'pasien'])->default('pasien');
             $table->string('nama');
             $table->string('alamat');
             $table->string('no_ktp')->unique();
